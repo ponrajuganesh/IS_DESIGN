3179745e51830cf690ec912e0194e3a1641423e6
ponrajuganesh <ponrajuganesh@live.com>
2017-02-27 13:47:08 -0600

    added get_subcriptions method

--- handycart/handycart/handycart.py
+++ handycart/handycart/handycart.py
@@ -18,8 +18,27 @@ app = Flask(__name__)
 app.config.from_object(__name__)
 app.config.from_envvar('HANDYCART_SETTINGS', silent=True)
 
-CATEGORIES = []
+DAYS = {
+	0: 'M',
+	1: 'T',
+	2: 'W',
+	3: 'T',
+	4: 'F',
+	5: 'Sa',
+	6: 'Su'
+}
 
+FREQUENCY = {
+	0: 'Weekly',
+	1: 'Bi-Weekly',
+	2: 'Monthly'
+}
+
+UNITS = {
+	1: 'Oz',
+	2: 'Count',
+	3: 'lb'
+}
 def get_db():
 	"""Opens a new database connection if there is none yet for the
 	current application context.
@@ -94,9 +113,61 @@ def subscribe_product():
 
 @app.route('/add_subscription', methods=['POST'])
 def add_subscription():
+	print ("COMING HERE", file=sys.stderr)
 	frequency, days, price_id = request.form['frequency'], request.form['days'], request.form['price_id']
-
+	print("frequency " + str(frequency), file=sys.stderr)
+	print("days " + str(days), file=sys.stderr)
+	print("price_id " + str(price_id), file=sys.stderr)
+	print("user_id " + str(session["user_id"]), file=sys.stderr)
 	db = get_db()
 	db.execute("insert into subscription (user_id, price_id, days, frequency) values (?, ?, ?, ?)", [session['user_id'], price_id, days, frequency])
 	db.commit()
-	return 'Done!'
+	# return redirect(url_for('get_products', category_id="3"))
+	return "Inserted!"
+
+@app.route('/get_subscriptions')
+def get_subscriptions():
+	user_id = session['user_id']
+
+	subscriptions_raw_data = query_db("select * from subscription where user_id = ?", [user_id])
+	processed_subscriptions = []
+	for subscription in subscriptions_raw_data:
+		processed_subscription = {}
+
+		price = query_db("select * from price where id = ?", [subscription['price_id']], one=True)
+		seller = query_db("select * from seller where id = ?", [price['seller_id']], one=True)
+		product = query_db("select * from product where id = ?", [price['product_id']], one=True)
+
+		processed_subscription['product_name'] = None
+		processed_subscription['product_name'] = product['name']
+
+		processed_subscription['cost'] = None
+		processed_subscription['cost'] = price['cost']
+
+		processed_subscription['quantity'] = None
+		processed_subscription['quantity'] = price['quantity']
+
+		processed_subscription['units'] = None
+		processed_subscription['units'] = UNITS[product['units_id']]
+
+		processed_subscription['seller_name'] = None
+		processed_subscription['seller_name'] = seller['name']
+
+		day_numbers = []
+		day_numbers = subscription['days'].split(',')
+		days = []
+		for day_number in day_numbers:
+			days.append(DAYS[int(day_number)])
+
+		processed_subscription['days'] = None
+		processed_subscription['days'] = days
+
+		processed_subscription['frequency'] = None
+		processed_subscription['frequency'] = FREQUENCY[subscription['frequency']]
+
+		processed_subscription['img_src'] = None
+		processed_subscription['img_src'] = product['img_src']
+
+		processed_subscriptions.append(processed_subscription)
+
+	return render_template('subscription_list.html', subscriptions=processed_subscriptions)
