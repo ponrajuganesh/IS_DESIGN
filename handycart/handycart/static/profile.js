46dc0ce064719260ec8daca901ef7439aee99ecd
ponrajuganesh <ponrajuganesh@live.com>
2017-03-02 22:32:14 -0600

    Profile JS

---
+++ handycart/handycart/static/profile.js
@@ -0,0 +1,27 @@
+$(document).ready(function() {
+  var data = {}
+
+  data["username"] = $("#username").val();
+  data["email"] = $("#email").val();
+  data["phone"] = $("#phone").val();
+  data["street"] = $("#street").val();
+  data["apt_number"] = $("#apt_number").val();
+  data["city"] = $("#city").val();
+  data["zip"] = $("#zip").val();
+  data["state"] = $("#state").val();
+
+  if ($("#first_name")) {
+    data['first_name'] = $("#first_name").val();
+  }
+
+  if ($("#last_name")) {
+    data['last_name'] = $("#last_name").val();
+  }
+
+  $.getJSON($SCRIPT_ROOT + '/update_profile', {
+    data: JSON.stringify(data),
+  }, function(data) {
+    console.log(data);
+  });
+
+})
